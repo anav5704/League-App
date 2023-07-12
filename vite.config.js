a6d767e0a6721960bda8bb567e82c0eaf8dbabba
Anav Chand <anavkki@gmail.com>
2023-07-13 11:11:55 +1200

    r

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() ,
+    VitePWA({ 
+        registerType: 'autoUpdate',
+            manifest: {
+                name: "League of Losers HUB",
+                short_name: "LOL HUB",
+                start_url: ".",
+                background_color: "#ffffff",
+                theme_color: "#ffffff",
+                display: "standalone",
+                icons: [
+                    {
+                        src: "L192.png",
+                        sizes: "192x192",
+                        type: "image/png"
+                    },
+                    {
+                        src: "L512.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    }
+                ]
+            }
+     })],
 })
