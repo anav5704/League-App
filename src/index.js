9f0ef5206511d60c692ebb5b6ed22bf6b9159f1a
Anav Chand <anavkki@gmail.com>
2023-07-12 21:22:36 +1200

    j

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator){
-    navigator.serviceWorker.register("./serviceWorker.js").then(registration => {
+    navigator.serviceWorker.register("serviceWorker.js").then(registration => {
         console.log("Service Worker Registered ✅", registration )}
     ).catch(err => {
-        console.log("Service Worker Registratio Failed ❌", err )
+        console.log("Service Worker Registration Failed ❌", err )
     })
 } 
