1ba07f65b19cc054e2552ce0d957364681316ed2
Anav Chand <anavkki@gmail.com>
2023-07-13 08:44:22 +1200

    v

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator){
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register("/src/serviceWorker.js").then(registration => {
+        navigator.serviceWorker.register("./src/serviceWorker.js").then(registration => {
             console.log("Service Worker Registered ✅", registration )}
         ).catch(err => {
             console.log("Service Worker Registration Failed ❌", err )
