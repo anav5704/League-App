6196a3697b64571601810b927951223699a8aa29
Anav Chand <anavkki@gmail.com>
2023-07-30 18:06:49 +1200

    :)

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,27 +1,43 @@
+import { useEffect , useState} from "react";
 import Image from "../components/Image"
+import Loading from "./Loading"
+
 
 function Gallery(){
+    const [images, setImages] = useState([]);
+    const loadingImages = [
+        { src: "https://picsum.photos/500/250" , landscape: true } ,
+        { src: "https://picsum.photos/250/500" , landscape: false } ,
+        { src: "https://picsum.photos/250/500" , landscape: false } ,
+        { src: "https://picsum.photos/500/250" , landscape: true } ,
+        { src: "https://picsum.photos/500/250" , landscape: true } ,
+        { src: "https://picsum.photos/250/500" , landscape: false } ,
+        { src: "https://picsum.photos/250/500" , landscape: false } ,
+        { src: "https://picsum.photos/500/250" , landscape: true } ,
+        { src: "https://picsum.photos/250/500" , landscape: false } ,
+        { src: "https://picsum.photos/500/250" , landscape: true } ,
+        { src: "https://picsum.photos/500/250" , landscape: true } ,
+        { src: "https://picsum.photos/250/500" , landscape: false } ,
+        { src: "https://picsum.photos/250/500" , landscape: false } 
+    ];
 
-    const arr =[10]
+    useEffect(() => {
+        setTimeout(() => {
+            setImages(prevImages => [...prevImages, ...loadingImages]);
+            console.log("Images have been loaded!")
+        }, 5000)
+    }, [])
 
     return(
+        images.length ?
         <main className="p-5 h-full relative overflow-hidden overflow-y-scroll">
             <h1 className="text-2xl leading-none mb-5">Gallery</h1>
-            <div className=" w-full grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
-                <Image />
+            <div className=" w-full grid grid-cols-1 gap-5 md:grid-cols-4 lg:grid-cols-8">
+               { images.map((img, index) =>  <Image key={index} img={ img } />)}
             </div>
         </main>
+        :
+        <Loading />
     )
 }
 
